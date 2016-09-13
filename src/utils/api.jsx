6f900e14c425c0f17da46fa1d983b27c9b9d75a2
yichiang <yi_chiang@outlook.com>
2016-09-13 12:10:35 -0700

    add gulp file

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=a3b3d94c43ef57372979ceade30d58d6';
+
+module.exports = {
+    get: function(place) {
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // No need for special headers
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        });
+    }
+};
